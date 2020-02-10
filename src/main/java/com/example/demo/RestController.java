ef66687d16c35d88def85324e8ed08ae59a3db15
ayaz.ahmed <ayaz.ahmed_@hotmail.com>
2020-02-10 17:07:25 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/RestController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+
+    @GetMapping("/hello")
+    public String getAllUsers() {
+        System.out.println("Hello world");
+        return "Hello";
+    }
+}
